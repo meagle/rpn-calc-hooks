2e9f170013a1063c21eb64278c1a829d3fd826f1
Mark Eagle <meagle@gmail.com>
2019-03-11 09:35:19 -0400

    Added Context

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const CalcContext = React.createContext(0);
+
 class App extends Component {
   render() {
     return (
